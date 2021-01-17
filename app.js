275476f17558e56bb26b24582e862c5edec4d117
Yuchi Su <astraiostor@gmail.com>
2021-01-17 19:08:18 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+const PORT = 3000
+
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
 })
 
-app.listen(3000, () => {
-  console.log('The app is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`The app is running on http://localhost:${PORT}`)
 })
 
